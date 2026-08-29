cd3ad5fbfe490237719aeafa217ef54139926aae
aurpine <12262539+aurpine@users.noreply.github.com>
2026-08-29 06:00:02 -0400

    Try proper constructor

--- core/object/object.cpp
+++ core/object/object.cpp
@@ -988,7 +988,7 @@ VariantCallCache Object::lookup_function_call(const StringName &p_method_name, C
 		return VariantCallCache();
 	}
 
-	ret = member->payload.method;
+	ret = VariantCallCache(member->payload.method);
 	p_error = Callable::CallError::CALL_OK;
 	return ret;
 	// return VariantCallCache();
